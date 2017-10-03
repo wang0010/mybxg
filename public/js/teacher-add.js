a0d521691cd5428f1043a28118b0062dcb55e39a
wangshuai <632038284@qq.com>
2017-10-03 23:08:34 +0800

    完成讲师添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -11,12 +11,31 @@ define(["jquery","template","util"],function($,template,util){
    	  		// 解析数据，渲染页面
    	  		data.result.operse = "编辑讲师"
    	  		var html = template("teacheraddTpl",data.result)
-   	  		$("#teacheraddInfo").html(html)
+   	  		$("#teacheraddInfo").html(html);
+   	  		setForm("/api/teacher/update")
    	  	}
    	  })
    }else {
    	  // 添加讲师
-   	    var html = template("teacheraddTpl",{operse:"添加讲师"})
-   	  	$("#teacheraddInfo").html(html)	  		
+   	   var html = template("teacheraddTpl",{operse:"添加讲师"});
+   	   $("#teacheraddInfo").html(html);  	
+   	   setForm("/api/teacher/add")
+  }
+
+  //表单添加和编辑
+  function setForm(url){
+  	$("#teacherBtn").click(function(){
+  		$.ajax({
+  			type:"post",
+  			url:url,
+  			data:$("#teacherForm").serialize(),
+  			dataType:"json",
+  			success:function(data){
+  				if(data.code == 200){
+  					location.href = "/teacher/list"
+  				}
+  			}
+  		})
+  	})
   }
 })
